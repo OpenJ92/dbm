d784c836e5336a22f668470f7dc754290b8187e1
OpenJ92 <jacob.vartuli.92@gmail.com>
2020-01-16 17:11:33 -0500

    documentation added to src/CONNECT.py

--- src/CONNECT.py
+++ src/CONNECT.py
@@ -5,22 +5,68 @@ from pandas import read_sql
 
 class CONNECT(object):
     def __init__(self, name):
+        """
+        __init__(self, name):
+        name::string - Name of database to connect to. Direct
+                        coresompndance to the outer most layer 
+                        of ~/.scd/config.yml dictionary.
+
+        
+        function - Store name of database connected to and load
+            make connection to affermentioned database.
+
+        returns - self
+        """
         self.name = name
         self._configuration = self._process_configuration()
 
     def __enter__(self):
-        print("__enter__")
+        """
+        __enter__(self):
+
+        function - Unpack configuration credentials for given database
+            name and store into instance _connection variable. For use
+            with python context manager element.
+
+        returns - self
+        """
         self._connection = connect(**self._configuration[self.name])
         return self
 
     def __exit__(self, exception_type, exception_value, traceback):
-        print("__exit__")
+        """
+        __exit__(self):
+
+        function - Close connection to given database. For use 
+            with python context manager element.
+
+        returns - None
+        """
         self._connection.close()
 
     def _process_configuration(self):
+        """
+        _process_configuration(self):
+
+        function: Through the use of yaml.load function and the 
+            FullLoader flag, process .scd/config.yml file into
+            python dictionary. For use in the __enter__ method.
+
+        returns - dict
+        """
         configuration = f"{expanduser('~')}/.scd/config.yml"
         with open(configuration, 'r') as c:
             return load(c, Loader=FullLoader)
 
     def _extract(self, sql):
+        """
+        _extract(self, sql):
+        sql::string - sql statement to execute on database
+
+        function: Pull data from database into pandas DataFrame
+            object. For our purpose, this will be used to query
+            the snowflake information schema view of columns.
+
+        returns - pandas.DataFrame
+        """
         return read_sql(sql, self._connection)
