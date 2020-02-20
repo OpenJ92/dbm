618799cd479242f7acaa51f1ea3e8188fcb38a01
OpenJ92 <jacob.vartuli.92@gmail.com>
2020-02-20 12:58:22 -0500

    test DBT class

--- __main__.py
+++ __main__.py
@@ -4,10 +4,12 @@ from src.OBJECTS.DATABASE.DATABASE import DATABASE
 from src.OBJECTS.ACTIONS.CHECK.context import CHECK
 from src.OBJECTS.ACTIONS.POLICY.context import POLICY
 from src.OBJECTS.UNARY.CHANGE_REPORT import CHANGE_REPORT
+from src.OBJECTS.UNARY.DBT import DBT
 
 if __name__ == '__main__':
     with CONNECT('RAW') as raw:
         data = DATABASE(raw, ACTION = [CHECK, POLICY])
         # CHANGE_REPORT(data)
+        DBT(data)
 
 # access to global macros https://stackoverflow.com/questions/5034437/accessing-global-attributes-from-inside-a-macro-in-jinja2
