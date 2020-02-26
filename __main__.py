651dcca1da2b15160cbc59ad713538307195c4ba
OpenJ92 <jacob.vartuli.92@gmail.com>
2020-02-26 17:10:07 -0500

    update __main__ to reflect changes in previous iteration

--- __main__.py
+++ __main__.py
@@ -1,14 +1,14 @@
 from src.CONNECT import CONNECT
 from src.OBJECTS.DATABASE.DATABASE import DATABASE
 
-from src.OBJECTS.ACTIONS.CHECK.context import CHECK
+from src.OBJECTS.ACTIONS.LOCATE.context import LOCATE
 from src.OBJECTS.ACTIONS.POLICY.context import POLICY
 from src.OBJECTS.UNARY.CHANGE_REPORT import CHANGE_REPORT
 from src.OBJECTS.UNARY.DBT import DBT
 
 if __name__ == '__main__':
     with CONNECT('RAW') as raw:
-        data = DATABASE(raw, ACTION = [CHECK, POLICY])
+        data = DATABASE(raw, ACTION = [LOCATE, POLICY])
         # CHANGE_REPORT(data)
         DBT(data)
 
