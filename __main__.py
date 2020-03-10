34a0eb315cc8ba30ee0747ca9a4299998cd207bd
OpenJ92 <jacob.vartuli.92@gmail.com>
2020-03-10 17:19:36 -0400

    _

--- __main__.py
+++ __main__.py
@@ -10,6 +10,7 @@ from src.OBJECTS.UNARY.CHANGE_REPORT import CHANGE_REPORT
 if __name__ == '__main__':
     with CONNECT('RAW') as raw:
         data = DATABASE(raw, ACTION = [LOCATE, POLICY], POST_ACTION = [DBT, LOOKER])
-        # CHANGE_REPORT(data)
+        CHANGE_REPORT(data)
+        __import__('pdb').set_trace()
 
 # access to global macros https://stackoverflow.com/questions/5034437/accessing-global-attributes-from-inside-a-macro-in-jinja2
